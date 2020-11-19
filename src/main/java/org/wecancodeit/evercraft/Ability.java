350b9c42b061ce3ac96371bda9512cf50788abc1
David Trimmer <dbtrimmer1@gmail.com>
2020-11-19 01:59:39 -0500

    score cannot be more than 21

--- src/main/java/org/wecancodeit/evercraft/Ability.java
+++ src/main/java/org/wecancodeit/evercraft/Ability.java
@@ -1,15 +1,23 @@
 package org.wecancodeit.evercraft;
 
+import java.util.EmptyStackException;
+
 public class Ability {
 
     private int score = 10;
 
+
     public int getScore() {
         return score;
     }
+
     public void setScore(int score) {
-        this.score = score;
+        if (score > 20) { score = 20; }
+
+        { this.score = score; }
     }
-
-
 }
+
+
+
+
--- src/test/java/org/wecancodeit/evercraft/AbilityTest.java
+++ src/test/java/org/wecancodeit/evercraft/AbilityTest.java
@@ -3,6 +3,7 @@ package org.wecancodeit.evercraft;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AbilityTest {
@@ -16,12 +17,19 @@ public class AbilityTest {
     }
 
     @Test
-    public void scoreDefaultShouldBe10(){
+    public void scoreDefaultShouldBe10() {
         assertEquals(10, underTest.getScore());
     }
+
     @Test
-    public void scoreCanChange(){
+    public void scoreCanChange() {
         underTest.setScore(15);
         assertEquals(15, underTest.getScore());
+    }
+
+    @Test
+    public void scoreCannotBeMoreThan21(){
+        underTest.setScore(21);
+        assertThat(underTest.getScore()).isEqualTo(20);
     }
 }
