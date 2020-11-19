76da0089829d33eed8355662e9a4de1da7bd3254
David Trimmer <dbtrimmer1@gmail.com>
2020-11-18 21:04:05 -0500

    refactored Attack class and resolve method

--- src/main/java/org/wecancodeit/evercraft/Attack.java
+++ src/main/java/org/wecancodeit/evercraft/Attack.java
@@ -1,11 +1,18 @@
 package org.wecancodeit.evercraft;
 
 public class Attack {
+
+    private Hero attacker;
+    private Hero defender;
+
     public Attack(Hero attacker, Hero defender) {
+
+        this.attacker = attacker;
+        this.defender = defender;
     }
 
     public boolean resolve(int roll){
 
-        return roll > 10;
+        return roll > defender.getArmorClass();
     }
 }
